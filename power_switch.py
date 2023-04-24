d89ecbeb90b7aa3a569ce7d09d4da118aafec6da
Fabien Haff <41027796+fabien-haff@users.noreply.github.com>
2023-04-24 21:08:07 +0200

    Import project

---
+++ power_switch.py
@@ -0,0 +1,34 @@
+from time import sleep
+import requests
+import sys
+from requests.auth import HTTPDigestAuth
+
+auth = HTTPDigestAuth('admin', 'admin')
+
+""" CMD codification 
+    0_0_0 
+    first digit is unused 
+    second digit is outlet number starting from 0
+    third digit is action, 0 = 0N, 1 = OFF, 2 = Reboot
+"""
+
+
+
+def control_outlet(outlet_number, action):
+    action_num=0
+    number = outlet_number -1
+    match action:
+        case "ON":
+            action_num = 0
+        case "OFF":
+            action_num = 1
+        case "RST":
+            action_num = 2
+        case _:
+            exit(555)
+    url = f"http://192.168.1.45/admin/control.cgi?CMD=0_{number}_{action_num}"
+    requests.get(url, auth=auth)
+
+control_outlet(int(sys.argv[1]),sys.argv[2])
+
+
